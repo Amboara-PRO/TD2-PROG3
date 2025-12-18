fc3d41feb1969011f9487da95e93fc5d794454db
Amboara <andriamalalaamboarafaneva@gmail.com>
2025-12-18 23:44:14 +0300

    add DataRetriever and methods

---
+++ src/main/java/TD/DataRetriever.java
@@ -0,0 +1,26 @@
+package TD;
+
+import java.util.List;
+
+public class DataRetriever {
+    public Team findTeamById(Integer id){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public List<Player> findPlayers(int page, int size){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public List<Player> createPlayers(List<Player> newPlayers){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public Team saveTeam(Team teamToSave){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public List<Team> findTeamsByPlayerName(String playerName){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public List<Player> findPlayersByCriteria(String playerName,
+                                            PlayerPositionEnum position, String teamName, ContinentEnum
+                                            continent, int page, int size){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
